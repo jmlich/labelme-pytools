e9f1ebac667d73b6bfda77fe21408d277855c3d7
Jozef Mlich <imlich@fit.vutbr.cz>
2019-03-19 12:46:41 +0100

    add some resizing

---
+++ crop_orig_labels.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#IN=Images
+#OUT=FIRE_512
+
+#prepare_ds () {
+
+#    infile="$1"
+#    indir=$(dirname "$infile")
+#    outfile=$(echo $infile | sed "s/$IN/$OUT/g")
+#    outdir=$(echo $indir | sed "s/$IN/$OUT/g")
+
+#    mkdir -p $outdir
+#    convert $infile -auto-orient -resize 1000x1000 -gravity Center -crop 512x512+0+0 +repage $outfile;
+
+    echo $infile
+#    echo $infile $outfile $indir $outdir
+#}
+
+
+#!/bin/bash
+imgdir='/home/jmlich/workspace/fire/labelme/LabelMeAnnotationTool/Images'
+xmldir='/home/jmlich/workspace/fire/labelme/LabelMeAnnotationTool/Annotations'
+outdir='/var/www/html/fire/dataset-fire-labelme-cropped-Images'
+maskdir='/var/www/html/fire/dataset-fire-labelme-mask'
+cmaskdir='/var/www/html/fire/dataset-fire-labelme-cropped-Mask'
+
+msum=0
+isum=0
+# foreach xml in directory
+while IFS= read -r -d $'\0' xml; do
+    xml=$(realpath "$xml")
+
+    bname=$(echo $xml | sed "s|^$xmldir/||" | sed 's|\.xml$||')
+    # find corresponding jpg in directory
+    img="$imgdir/${bname}.jpg"
+    out="$outdir/${bname}.jpg"
+    mask="$maskdir/${bname}.png"
+    cmask="$cmaskdir/${bname}.png"
+
+    outdirfull="$(dirname "$out")"
+    maskdirfull="$(dirname "$mask")"
+    cmaskdirfull="$(dirname "$cmask")"
+
+
+    if [ ! -f "$img" ]; then
+        echo "Error: Image '$img' doesn't exists..." >&2
+#        rm -f "$xml"
+        continue
+    fi
+
+    if [ ! -f "$mask" ]; then
+        echo "Error: mask '$mask' doesn't exits..." >&2
+        continue
+    fi
+
+    mkdir -p "$outdirfull"
+    mkdir -p "$maskdirfull"
+    mkdir -p "$cmaskdirfull"
+
+    if [ ! -f "$cmask" ]; then
+#        convert $mask -auto-orient -resize 1000x1000 -gravity Center -crop 512x512+0+0 +repage $cmask;
+        convert "$mask" -resize 512x512 "$cmask";
+        msum=$((msum + 1))
+    fi
+
+    if [ ! -f "$out" ]; then
+#        convert $img -auto-orient -resize 1000x1000 -gravity Center -crop 512x512+0+0 +repage $out;
+        convert "$img" -resize 512x512 "$out";
+        isum=$((isum + 1))
+    fi
+
+    echo -n "+"
+
+done < <(find "$xmldir" -type f -name '*.xml' -print0)
+
+echo ""
+echo "isum = $isum; msum = $msum;"
+
+
--- save_all_labels.sh
+++ save_all_labels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 imgdir='/home/jmlich/workspace/fire/labelme/LabelMeAnnotationTool/Images'
 xmldir='/home/jmlich/workspace/fire/labelme/LabelMeAnnotationTool/Annotations'
-outdir='/var/www/html/fire/dataset-fire-labelme'
+outdir='/var/www/html/fire/dataset-fire-labelme-with-poly'
 maskdir='/var/www/html/fire/dataset-fire-labelme-mask'
 mkdir -p "$outdir"
 mkdir -p "$maskdir"
@@ -9,6 +9,8 @@ mkdir -p "$maskdir"
 rm -f "colors_mask.json"
 rm -f "colors.json"
 
+sum=0
+sum_out=0
 # foreach xml in directory
 while IFS= read -r -d $'\0' xml; do
     xml=$(realpath "$xml")
@@ -33,16 +35,22 @@ while IFS= read -r -d $'\0' xml; do
     mkdir -p "$maskdirfull"
 
     if [ -f "$out" ]; then
+        sum_out=$((sum_out + 1))
         echo -n "+"
         continue;
     fi
 
     if [ -f "$mask" ]; then
+        sum_out=$((sum_out + 1))
         echo -n "*"
         continue;
     fi
 
     ./save_labelme.py "$img" "$xml" "$out" "$mask" #> /dev/null
+    sum=$((sum + 1))
 
 done < <(find "$xmldir" -type f -name '*.xml' -print0)
 
+
+echo ""
+echo "skipped = $sum_out; converted = $sum"
